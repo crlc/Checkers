410417d6f8a80d0fd5b69fc00fc7512826da4f3a
Carlos Carrera <crlc@me.com>
2015-04-30 10:51:05 -0700

    returns directions a piece could move in

--- piece.rb
+++ piece.rb
@@ -1,15 +1,28 @@
 class Piece
 
-  def initialize(color)
+  def initialize(color, initial_position)
     @king = false
     @color = color
+    @pos = initial_position
   end
 
   def move_diffs
-    [[1,1],[1,-1]]
+    array = [[1,1],[1,-1]]
+    if @king
+      array = array + array.map {|row, col| [-row, -col]}
+    else
+      array.map! {|row, col| [-row, -col]} unless @color == :red
+    end
+
+    array
   end
 
-  def perform_slide
+  def perform_slide(end_pos)
+    moves = move_diffs.select {|row, col| (row + @pos.first), (col + @post.last)}
+    if moves.include?(end_pos)
+      #move
+    else
+      #return error and retry
   end
 
   def perform_jump
