585d712ba91032c025dcdebd88f38cba91fa91f1
Carlos Carrera <crlc@me.com>
2015-04-30 11:18:52 -0700

    king a piece

--- piece.rb
+++ piece.rb
@@ -17,15 +17,38 @@ class Piece
     array
   end
 
-  def perform_slide(end_pos)
-    moves = move_diffs.select {|row, col| (row + @pos.first), (col + @post.last)}
-    if moves.include?(end_pos)
-      #move
-    else
-      #return error and retry
-  end
-
   def perform_jump
+    positions = move_diffs.map {|row, col| [row * 2, col * 2]}
 
   end
+
+  def perform_slide(end_pos)
+    #select the moves that are on the board have no one on them
+    positions = move_diffs.map {|row, col| (row + @pos.first), (col + @post.last)}
+    if positions.include?(end_pos)
+      @board[end_pos], @board[@pos] = @board[@pos], nil #move the piece if a valid move
+      @pos = end_pos
+    else
+      raise InvalidMoveError "Not a valid move"
+    end
+    maybe_promote
+  end
+
+  protected
+
+  def back_row?
+    case @color
+    when :white
+      @pos.first == 0
+    when :red
+      @pos.first == 7
+    end
+  end
+
+  def maybe_promote
+    @king = true if back_row?
+  end
+
 end
+
+class InvalidMoveError < StandardError ; end
